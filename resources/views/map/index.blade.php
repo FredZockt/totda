e44fdb9c7ab252de696d7394179f9c825ba82bbd
Frederik <frederik.borck@googlemail.com>
2023-01-30 23:33:31 +0100

    better map generation

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -86,6 +86,54 @@
                         @foreach($cities as $index => $city)
                             @if($city->x == $indexX && $city->y == $indexY)
                                 <span data-toggle="modal" data-target="#cityModal{{$index}}" data-x="{{$city->x}}" data-y="{{$city->y}}" style="cursor: pointer;display: block; height:8px; width: 8px; background-color: {{$city->kingdom_id == 1 ? 'red' : 'blue'}};"></span>
+                                <div class="modal fade" id="cityModal{{$index}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel{{$index}}" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel{{$index}}">{{ $city->name }}</h5>
+                                                <button type="button" class="close btn" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <table class="table">
+                                                    <thead>
+                                                    <tr>
+                                                        <th>Kingdom</th>
+                                                        <th>Distance</th>
+                                                        <th>Duration</th>
+                                                        <th>Arrival at</th>
+                                                    </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                    <tr>
+                                                        <td>{{ $city->kingdom }}</td>
+                                                        @if(!auth()->user()->job_id)
+                                                            <td>{{ auth()->user()->current_city_id && $city->id == auth()->user()->current_city_id ? '-' : number_format($city->distanceToInKm, 2) }}</td>
+                                                            <td>{{ $city->id == auth()->user()->current_city_id ? '-' : $city->distanceToAsReadable }}</td>
+                                                            <td>{{ $city->id == auth()->user()->current_city_id ? '-' : $city->distanceToAsDate }}</td>
+                                                        @else
+                                                            <td>-</td>
+                                                            <td>-</td>
+                                                            <td>-</td>
+                                                        @endif
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <div class="modal-footer">
+                                                @if(!auth()->user()->job_id)
+                                                    <form method="POST" action="/walk/{{$city->id}}">
+                                                        @csrf
+                                                        <input type="submit" value="visit" class="btn">
+                                                    </form>
+                                                @else
+                                                    <div class="alert alert-info">You are currently working/walking</div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endif
                         @endforeach
                     </div>
